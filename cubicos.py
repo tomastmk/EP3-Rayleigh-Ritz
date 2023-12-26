c126d54a4e87be13535ff34d7836f4e346eadedd
tomastmk <tomasamadio@gmail.com>
2023-12-26 12:25:23 -0300

    Limpando código 1

--- cubicos.py
+++ cubicos.py
@@ -1,3 +1,19 @@
+from solve_SDP.main import system_solve  #EP2
+import matplotlib.pyplot as plt
+import numpy as np
+import sympy as sp
+
+class bcolors:
+    OKBLUE    = '\033[94m'
+    OKGREEN   = '\033[92m'
+    WARNING   = '\033[93m'
+    RED = "\033[0;31m"
+    BROWN = "\033[0;33m"
+    PURPLE = "\033[0;35m"
+    CYAN = "\033[0;36m"
+    BOLD = "\033[1m"
+    ENDC = "\033[0m"
+
 def gauss_3_pontos(m,xi,i,j):
     
     h = H
@@ -9,6 +25,20 @@ def gauss_3_pontos(m,xi,i,j):
     return P1 + P2 + P3
 
 
+def m_ap(u, xi, i, j):
+    
+    h = H
+    x = X
+    
+    return p( u*h/2 + x[xi]/2 + x[xi+1]/2 ) * g_derivada(i, u*h/2 + x[xi]/2 + x[xi+1]/2 ) * g_derivada(j, u*h/2 + x[xi]/2 + x[xi+1]/2 )
+
+def m_aq(u, xi, gi, gj):
+    
+    h = H
+    x = X
+    
+    return q( u*h/2 + x[xi]/2 + x[xi+1]/2 ) * g(gi, u*h/2 + x[xi]/2 + x[xi+1]/2 ) * g(gj, u*h/2 + x[xi]/2 + x[xi+1]/2 )
+
 def m_d(u , xi, bi, j):
     
     h = H
@@ -16,32 +46,6 @@ def m_d(u , xi, bi, j):
     
     
     return f( u*h/2 + x[xi]/2 + x[xi+1]/2 )*B( bi , u*h/2 + x[xi]/2 + x[xi+1]/2 )
-    
-    
-def di(i):
-    
-    h = H
-    n = N
-    
-    if i == 0:
-        
-        return h/2 * (gauss_3_pontos(m_d, 0, 0,0) + gauss_3_pontos(m_d, 1 , 0,0)) - 2*h*gauss_3_pontos(m_d,0,-1,0)
-    
-    elif i == 1:
-        
-        return h/2 * (gauss_3_pontos(m_d,0,1,0)+gauss_3_pontos(m_d,1,1,0)+gauss_3_pontos(m_d,2,1,0)-gauss_3_pontos(m_d,0,-1,0))
-    
-    elif 2 <= i <= n-1:
-        
-        return h/2 * (gauss_3_pontos(m_d,i-2,i,0)+gauss_3_pontos(m_d,i-1,i,0)+gauss_3_pontos(m_d,i,i,0)+gauss_3_pontos(m_d,i+1,i,0))
-    
-    elif i == n:
-        
-        return h/2 * (gauss_3_pontos(m_d,n-2,n,0)+gauss_3_pontos(m_d,n-1,n,0)+gauss_3_pontos(m_d,n,n,0)+gauss_3_pontos(m_d,n,n+2,0))
-
-    elif i == n+1:
-        
-        return h/2 * (gauss_3_pontos(m_d,n-1,n,0)+gauss_3_pontos(m_d,n,n,0)) - 2*h*gauss_3_pontos(m_d,n,n+2,0)
     
     
 
@@ -63,23 +67,83 @@ def B(i,x):
     elif 1<t and t<=2:   return (1/4) * (2-t)**3
         
     else:   return 0
+
+def derivada_B(i,x):
+    a = 0
+    h = H
+    t = (x-a-i*h)/h
     
+    if t<=-2:   return 0
+    
+    elif -2<t and t<=-1:   return 3/4 * (2+t)**2
+        
+    elif -1<t and t<=0:   return 3/4 * (2+t)**2 - 3*(1+t)**2
+        
+    elif 0<t and t<=1:    return -3/4 * (2-t)**2 + 3*(1-t)**2
+        
+    elif 1<t and t<=2:   return -3/4 * (2-t)**2
+        
+    else:   return 0
 
 
-def m_ap(u, xi, i, j):
+def g(i,x):
     
+    n = N
+    
+    if i == 0:
+        return B(0,x) - 4*B(-1,x)
+        
+    elif i == 1:
+        return B(1,x) - B(-1,x)
+    
+    elif 2<=i and i<=n-1:
+        return B(i,x)
+        
+    elif i == n:
+        return B(n,x) - B(n+2,x)
+
+    elif i ==n+1:
+        return B(n+1,x) - 4*B(n+2,x)
+    
+def g_derivada(i,x):
+    
+    n = N
+    
+    if i == 0:
+        return derivada_B(0,x) - 4*derivada_B(-1,x)
+        
+    elif i == 1:
+        return derivada_B(1,x) - derivada_B(-1,x)
+    
+    elif 2<=i and i<=n-1:
+        return derivada_B(i,x)
+        
+    elif i == n:
+        return derivada_B(n,x) - derivada_B(n+2,x)
+
+    elif i ==n+1:
+        return derivada_B(n+1,x) - 4*derivada_B(n+2,x)    
+           
+
+    
+def matrix_A():
+
+    n = N
     h = H
     x = X
     
-    return p( u*h/2 + x[xi]/2 + x[xi+1]/2 ) * g_derivada(i, u*h/2 + x[xi]/2 + x[xi+1]/2 ) * g_derivada(j, u*h/2 + x[xi]/2 + x[xi+1]/2 )
-
-def m_aq(u, xi, gi, gj):
+    A = [ [],[],[],[] ]
     
-    h = H
-    x = X
+    for i in range(n):
+        A[0].append(aij(i,i))
+    for i in range(n-1):
+        A[1].append(aij(i,i+1))
+    for i in range(n-2):
+        A[2].append(aij(i,i+2))
+    for i in range(n-3):
+        A[3].append(aij(i,i+3))
     
-    return q( u*h/2 + x[xi]/2 + x[xi+1]/2 ) * g(gi, u*h/2 + x[xi]/2 + x[xi+1]/2 ) * g(gj, u*h/2 + x[xi]/2 + x[xi+1]/2 )
-
+    return A
 
 def aij(i,j):
 
@@ -113,80 +177,6 @@ def aij(i,j):
 
 
 
-def g(i,x):
-    
-    n = N
-    
-    if i == 0:
-        return B(0,x) - 4*B(-1,x)
-        
-    elif i == 1:
-        return B(1,x) - B(-1,x)
-    
-    elif 2<=i and i<=n-1:
-        return B(i,x)
-        
-    elif i == n:
-        return B(n,x) - B(n+2,x)
-
-    elif i ==n+1:
-        return B(n+1,x) - 4*B(n+2,x)
-    
-  
-def g_derivada(i,x):
-    
-    n = N
-    
-    if i == 0:
-        return derivada_B(0,x) - 4*derivada_B(-1,x)
-        
-    elif i == 1:
-        return derivada_B(1,x) - derivada_B(-1,x)
-    
-    elif 2<=i and i<=n-1:
-        return derivada_B(i,x)
-        
-    elif i == n:
-        return derivada_B(n,x) - derivada_B(n+2,x)
-
-    elif i ==n+1:
-        return derivada_B(n+1,x) - 4*derivada_B(n+2,x)    
-           
-           
-def derivada_B(i,x):
-    a = 0
-    h = H
-    t = (x-a-i*h)/h
-    
-    if t<=-2:   return 0
-    
-    elif -2<t and t<=-1:   return 3/4 * (2+t)**2
-        
-    elif -1<t and t<=0:   return 3/4 * (2+t)**2 - 3*(1+t)**2
-        
-    elif 0<t and t<=1:    return -3/4 * (2-t)**2 + 3*(1-t)**2
-        
-    elif 1<t and t<=2:   return -3/4 * (2-t)**2
-        
-    else:   return 0
-
-
-from solve_SDP.main import system_solve  #EP2
-import matplotlib.pyplot as plt
-import numpy as np
-import sympy as sp
-
-class bcolors:
-    OKBLUE    = '\033[94m'
-    OKGREEN   = '\033[92m'
-    WARNING   = '\033[93m'
-    RED = "\033[0;31m"
-    BROWN = "\033[0;33m"
-    PURPLE = "\033[0;35m"
-    CYAN = "\033[0;36m"
-    BOLD = "\033[1m"
-    ENDC = "\033[0m"
-
 def matrix_D():
     
     x = X
@@ -202,25 +192,31 @@ def matrix_D():
     
     return D
   
-
-def matrix_A():
-
-    n = N
+def di(i):
+    
     h = H
-    x = X
+    n = N
     
-    A = [ [],[],[],[] ]
+    if i == 0:
+        
+        return h/2 * (gauss_3_pontos(m_d, 0, 0,0) + gauss_3_pontos(m_d, 1 , 0,0)) - 2*h*gauss_3_pontos(m_d,0,-1,0)
     
-    for i in range(n):
-        A[0].append(aij(i,i))
-    for i in range(n-1):
-        A[1].append(aij(i,i+1))
-    for i in range(n-2):
-        A[2].append(aij(i,i+2))
-    for i in range(n-3):
-        A[3].append(aij(i,i+3))
+    elif i == 1:
+        
+        return h/2 * (gauss_3_pontos(m_d,0,1,0)+gauss_3_pontos(m_d,1,1,0)+gauss_3_pontos(m_d,2,1,0)-gauss_3_pontos(m_d,0,-1,0))
     
-    return A
+    elif 2 <= i <= n-1:
+        
+        return h/2 * (gauss_3_pontos(m_d,i-2,i,0)+gauss_3_pontos(m_d,i-1,i,0)+gauss_3_pontos(m_d,i,i,0)+gauss_3_pontos(m_d,i+1,i,0))
+    
+    elif i == n:
+        
+        return h/2 * (gauss_3_pontos(m_d,n-2,n,0)+gauss_3_pontos(m_d,n-1,n,0)+gauss_3_pontos(m_d,n,n,0)+gauss_3_pontos(m_d,n,n+2,0))
+
+    elif i == n+1:
+        
+        return h/2 * (gauss_3_pontos(m_d,n-1,n,0)+gauss_3_pontos(m_d,n,n,0)) - 2*h*gauss_3_pontos(m_d,n,n+2,0)
+
 
 
 def f(x):
@@ -315,7 +311,6 @@ def main():
 
     plt.show()
     
-    
-    
+
     
 main()
